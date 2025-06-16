63c1117d03a1ff2337312d026a603d3008a390b0
leoff15 <45170727+leoff15@users.noreply.github.com>
2025-06-16 19:17:49 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 8080; // Alterado para porta 8080
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
