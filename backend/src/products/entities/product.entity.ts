3945c6b05c009257bfdf8eb9be20e1d68716f7f0
leoff15 <45170727+leoff15@users.noreply.github.com>
2025-06-16 18:52:56 -0300

    Update product.entity.ts

--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -3,24 +3,23 @@ import { UUIDV4 } from 'sequelize';
 
 @Table({ tableName: 'Product', createdAt: true, updatedAt: true })
 export class Product extends Model {
-	  @Column({
-		      type: DataType.UUID,
-		          primaryKey: true,
-			      defaultValue: UUIDV4,
-			          allowNull: false,
-				    })
-				      id: string;
+  @Column({
+    type: DataType.UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+    allowNull: false,
+  })
+  declare id: string;
 
-				        @Column({ allowNull: false, unique: true })
-					  name: string;
+  @Column({ allowNull: false, unique: true })
+  name: string;
 
-					    @Column({})
-					      price: number;
+  @Column({})
+  price: number;
 
-					        @Column({})
-						  category: string;
+  @Column({})
+  category: string;
 
-						    @Column({})
-						      rating: number;
+  @Column({})
+  rating: number;
 }
-
